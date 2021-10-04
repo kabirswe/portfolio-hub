5aa61ae47f5806ab95173669f0d3dfedbd4350f3
enohyn <shayokullah@gmail.com>
2021-10-04 14:34:18 +0600

    refactor footer content 4 th oct

--- src/containers/DefaultLayout/components/Footer/Footer.js
+++ src/containers/DefaultLayout/components/Footer/Footer.js
@@ -8,10 +8,21 @@ import Twitter from "./static/image/twitter.svg";
 import Linkedin from "./static/image/linkedin.svg";
 import FooterBg from "./static/image/footer-bg.jpg";
 import "./static/Footer.scss";
-import BrandLogo from './static/image/brandLogo.png'
+import BrandLogo from "./static/image/brandLogo.png";
+import Marker from "./static/image/marker.png";
+import RightArrow from "./static/image/right-arrow.svg";
 
-
-const usefulLinks=["Group Profiles","Vision & Values","History","Awards", "Leadership", "CSR", "Sustainability", "Careers", "Media"]
+const usefulLinks = [
+  "Group Profiles",
+  "Vision & Values",
+  "History",
+  "Awards",
+  "Leadership",
+  "CSR",
+  "Sustainability",
+  "Careers",
+  "Media",
+];
 const Footer = () => {
   return (
     <div className="main-footer">
@@ -21,63 +32,79 @@ const Footer = () => {
 			alt="top right bg"
 		/> */}
       <Container>
-       <div className="content-wrapper">
-          <div className="company-section">
-            <img src={BrandLogo}/>
-            <input type="text" placeholder="email"/>
-            <p>
-              Stay Always in touch.! Subscribe our newsletter
-            </p>
-          </div>
-          <div className="contact-section">
-            <h3>Contacts</h3>
-            <p>
-              19th bowery street new york 
-            </p>
-            <p>
-              email@email.com
-            </p>
-            <p>
-              +8880000114558
-            </p>
-            <div className="social-icons">
-              <a>
-                <img src={Facebook}/>
-              </a>
-              <a>
-                <img src={Twitter}/>
-              </a>
-              <a>
-                <img src={Linkedin}/>
-              </a>
+        <div className="footer-wrapper">
+          <div className="content-wrapper">
+            <div className="company-section">
+              <div className="company-section-logo">
+                <img src={BrandLogo} />
+              </div>
+              <div className="input-block">
+                <img src={RightArrow} alt="" />
+                <input type="text" placeholder="Email" />
+              </div>
+              <p>Stay Always in touch.! Subscribe our newsletter</p>
+            </div>
+            <div className="contact-section">
+              <h3>Contacts</h3>
+              <div className="contact-section-list">
+                <p>
+                  <span>
+                    <img src={Marker} />
+                  </span>
+                  19th bowery street new york
+                </p>
+                <p>email@email.com</p>
+                <p>+8880000114558</p>
+                <div className="social-icons">
+                  <a>
+                    <img src={Facebook} />
+                  </a>
+                  <a>
+                    <img src={Twitter} />
+                  </a>
+                  <a>
+                    <img src={Linkedin} />
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="useful-link-section">
+              <h3>Useful Links</h3>
+              <div className="useful-links">
+                <div className=" link-list1">
+                  <p>Group Profiles</p>
+                  <p> Vision & Values</p>
+                  <p> History</p>
+                  <p> Awards</p>
+                  <p> Awards</p>
+                </div>
+                <div className=" link-list2">
+                  <p>CSR</p>
+                  <a href="#">Sustainability</a>
+                  <p> Carriers</p>
+                  <p> Media</p>
+                  <p> Services</p>
+                </div>
+              </div>
+            </div>
+            <div className="latest-news">
+              <h3>Latest News</h3>
+              <div className="latest-news-list">
+                <p> Best 15 Secrets of digital Transformation</p>
+                <p> November 22 </p>
+                <p> Best 15 Secrets of digital Transformation</p>
+                <p> November 22 </p>
+              </div>
             </div>
           </div>
-          <div className="contact-section">
-            <h3>
-              Useful Links
-            </h3>
-            {usefulLinks.map((index) => (
-                <p>
-                  {index}
-                </p>
-            ))}
-            
-          </div>
-          <div className="latest-news">
-            <h3>Latest News</h3>
-            <p> Best 15 Secrets of digital Transformation</p>
-            <p> November 22 </p>
-            <p> Best 15 Secrets of digital Transformation</p>
-            <p> November 22 </p>
-          </div>
-       </div>
-       <div className="copyright-section">
+          <div className="copyright-section">
             <p>
-             Copyright &copy; 2020 the Pascal by <a> WebGeniusLab. </a> All rights reserved
+              Copyright &copy; 2020 the Pascal by <a> WebGeniusLab. </a> All
+              rights reserved
             </p>
           </div>
+        </div>
       </Container>
-      
     </div>
   );
 };
